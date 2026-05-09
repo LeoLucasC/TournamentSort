2723ab72a603dd350e662912631fdf9928a8dd1a
LeoLucasC <gloriaxd77@hotmail.com>
2026-05-09 00:29:36 -0500

    final

--- TournamentSort/Form1.Designer.vb
+++ TournamentSort/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -17,22 +17,262 @@ Partial Class Form1
     'Requerido por el Diseñador de Windows Forms
     Private components As System.ComponentModel.IContainer
 
-    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
-    'Se puede modificar usando el Diseñador de Windows Forms.  
-    'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    'Controles de la interfaz
+    Friend WithEvents pnlHeader As System.Windows.Forms.Panel
+    Friend WithEvents lblTitle As System.Windows.Forms.Label
+    Friend WithEvents lblGroup As System.Windows.Forms.Label
+    Friend WithEvents pnlSidebar As System.Windows.Forms.Panel
+    Friend WithEvents btnGenerateData As System.Windows.Forms.Button
+    Friend WithEvents btnStartTournament As System.Windows.Forms.Button
+    Friend WithEvents lblListTitle As System.Windows.Forms.Label
+    Friend WithEvents lstCountries As System.Windows.Forms.ListBox
+    Friend WithEvents pnlMain As System.Windows.Forms.Panel
+    Friend WithEvents lblTreeTitle As System.Windows.Forms.Label
+    Friend WithEvents tvTournament As System.Windows.Forms.TreeView
+    Friend WithEvents pnlLogs As System.Windows.Forms.Panel
+    Friend WithEvents lblLogsTitle As System.Windows.Forms.Label
+    Friend WithEvents lstLogs As System.Windows.Forms.TextBox
+
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.pnlHeader = New System.Windows.Forms.Panel()
+        Me.lblGroup = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.pnlSidebar = New System.Windows.Forms.Panel()
+        Me.lstCountries = New System.Windows.Forms.ListBox()
+        Me.lblListTitle = New System.Windows.Forms.Label()
+        Me.btnStartTournament = New System.Windows.Forms.Button()
+        Me.btnGenerateData = New System.Windows.Forms.Button()
+        Me.pnlMain = New System.Windows.Forms.Panel()
+        Me.tvTournament = New System.Windows.Forms.TreeView()
+        Me.lblTreeTitle = New System.Windows.Forms.Label()
+        Me.pnlLogs = New System.Windows.Forms.Panel()
+        Me.lstLogs = New System.Windows.Forms.TextBox()
+        Me.lblLogsTitle = New System.Windows.Forms.Label()
+        Me.pnlHeader.SuspendLayout()
+        Me.pnlSidebar.SuspendLayout()
+        Me.pnlMain.SuspendLayout()
+        Me.pnlLogs.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'pnlHeader
+        '
+        Me.pnlHeader.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(25, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.pnlHeader.Controls.Add(Me.lblGroup)
+        Me.pnlHeader.Controls.Add(Me.lblTitle)
+        Me.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top
+        Me.pnlHeader.Location = New System.Drawing.Point(0, 0)
+        Me.pnlHeader.Name = "pnlHeader"
+        Me.pnlHeader.Size = New System.Drawing.Size(1288, 95)
+        Me.pnlHeader.TabIndex = 0
+        '
+        'lblGroup
+        '
+        Me.lblGroup.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblGroup.AutoSize = True
+        Me.lblGroup.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblGroup.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(215, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblGroup.Location = New System.Drawing.Point(454, 58)
+        Me.lblGroup.Name = "lblGroup"
+        Me.lblGroup.Size = New System.Drawing.Size(433, 21)
+        Me.lblGroup.TabIndex = 1
+        Me.lblGroup.Text = "✨ Grupo: Sarai Romero, Leo Lucas y Gimena Vargas ✨"
+        Me.lblGroup.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTitle
+        '
+        Me.lblTitle.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Segoe UI Black", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.lblTitle.Location = New System.Drawing.Point(359, 12)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(694, 47)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "🏆🏆🏆 TOURNAMENT SORT 🏆🏆🏆"
+        Me.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'pnlSidebar
+        '
+        Me.pnlSidebar.BackColor = System.Drawing.Color.FromArgb(CType(CType(30, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.pnlSidebar.Controls.Add(Me.lstCountries)
+        Me.pnlSidebar.Controls.Add(Me.lblListTitle)
+        Me.pnlSidebar.Controls.Add(Me.btnStartTournament)
+        Me.pnlSidebar.Controls.Add(Me.btnGenerateData)
+        Me.pnlSidebar.Dock = System.Windows.Forms.DockStyle.Left
+        Me.pnlSidebar.Location = New System.Drawing.Point(0, 95)
+        Me.pnlSidebar.Name = "pnlSidebar"
+        Me.pnlSidebar.Padding = New System.Windows.Forms.Padding(15, 20, 15, 15)
+        Me.pnlSidebar.Size = New System.Drawing.Size(300, 575)
+        Me.pnlSidebar.TabIndex = 1
+        '
+        'lstCountries
+        '
+        Me.lstCountries.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lstCountries.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(25, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.lstCountries.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lstCountries.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstCountries.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.lstCountries.FormattingEnabled = True
+        Me.lstCountries.IntegralHeight = False
+        Me.lstCountries.ItemHeight = 20
+        Me.lstCountries.Location = New System.Drawing.Point(15, 185)
+        Me.lstCountries.Name = "lstCountries"
+        Me.lstCountries.Size = New System.Drawing.Size(270, 370)
+        Me.lstCountries.TabIndex = 4
+        '
+        'lblListTitle
+        '
+        Me.lblListTitle.AutoSize = True
+        Me.lblListTitle.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblListTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(215, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblListTitle.Location = New System.Drawing.Point(15, 155)
+        Me.lblListTitle.Name = "lblListTitle"
+        Me.lblListTitle.Size = New System.Drawing.Size(269, 21)
+        Me.lblListTitle.TabIndex = 2
+        Me.lblListTitle.Text = "🎲🎲 PAÍSES EN DESORDEN 🎲🎲"
+        '
+        'btnStartTournament
+        '
+        Me.btnStartTournament.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.btnStartTournament.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnStartTournament.Dock = System.Windows.Forms.DockStyle.Top
+        Me.btnStartTournament.FlatAppearance.BorderSize = 0
+        Me.btnStartTournament.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnStartTournament.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStartTournament.ForeColor = System.Drawing.Color.White
+        Me.btnStartTournament.Location = New System.Drawing.Point(15, 75)
+        Me.btnStartTournament.Name = "btnStartTournament"
+        Me.btnStartTournament.Size = New System.Drawing.Size(270, 55)
+        Me.btnStartTournament.TabIndex = 1
+        Me.btnStartTournament.Text = "¡INICIAR TORNEO!"
+        Me.btnStartTournament.UseVisualStyleBackColor = False
+        '
+        'btnGenerateData
+        '
+        Me.btnGenerateData.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.btnGenerateData.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnGenerateData.Dock = System.Windows.Forms.DockStyle.Top
+        Me.btnGenerateData.FlatAppearance.BorderSize = 0
+        Me.btnGenerateData.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnGenerateData.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGenerateData.ForeColor = System.Drawing.Color.White
+        Me.btnGenerateData.Location = New System.Drawing.Point(15, 20)
+        Me.btnGenerateData.Name = "btnGenerateData"
+        Me.btnGenerateData.Size = New System.Drawing.Size(270, 55)
+        Me.btnGenerateData.TabIndex = 0
+        Me.btnGenerateData.Text = " CARGAR PAÍSES "
+        Me.btnGenerateData.UseVisualStyleBackColor = False
+        '
+        'pnlMain
+        '
+        Me.pnlMain.BackColor = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(250, Byte), Integer))
+        Me.pnlMain.Controls.Add(Me.tvTournament)
+        Me.pnlMain.Controls.Add(Me.lblTreeTitle)
+        Me.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.pnlMain.Location = New System.Drawing.Point(300, 95)
+        Me.pnlMain.Name = "pnlMain"
+        Me.pnlMain.Padding = New System.Windows.Forms.Padding(20)
+        Me.pnlMain.Size = New System.Drawing.Size(678, 575)
+        Me.pnlMain.TabIndex = 2
+        '
+        'tvTournament
+        '
+        Me.tvTournament.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tvTournament.BackColor = System.Drawing.Color.White
+        Me.tvTournament.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.tvTournament.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tvTournament.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(150, Byte), Integer))
+        Me.tvTournament.Indent = 25
+        Me.tvTournament.ItemHeight = 25
+        Me.tvTournament.LineColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.tvTournament.Location = New System.Drawing.Point(20, 75)
+        Me.tvTournament.Name = "tvTournament"
+        Me.tvTournament.Size = New System.Drawing.Size(638, 475)
+        Me.tvTournament.TabIndex = 4
+        '
+        'lblTreeTitle
+        '
+        Me.lblTreeTitle.AutoSize = True
+        Me.lblTreeTitle.Font = New System.Drawing.Font("Segoe UI Black", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTreeTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(100, Byte), Integer), CType(CType(100, Byte), Integer))
+        Me.lblTreeTitle.Location = New System.Drawing.Point(154, 28)
+        Me.lblTreeTitle.Name = "lblTreeTitle"
+        Me.lblTreeTitle.Size = New System.Drawing.Size(266, 32)
+        Me.lblTreeTitle.TabIndex = 3
+        Me.lblTreeTitle.Text = "LLAVES DEL TORNEO "
+        '
+        'pnlLogs
+        '
+        Me.pnlLogs.BackColor = System.Drawing.Color.FromArgb(CType(CType(20, Byte), Integer), CType(CType(25, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.pnlLogs.Controls.Add(Me.lstLogs)
+        Me.pnlLogs.Controls.Add(Me.lblLogsTitle)
+        Me.pnlLogs.Dock = System.Windows.Forms.DockStyle.Right
+        Me.pnlLogs.Location = New System.Drawing.Point(978, 95)
+        Me.pnlLogs.Name = "pnlLogs"
+        Me.pnlLogs.Padding = New System.Windows.Forms.Padding(15, 20, 15, 15)
+        Me.pnlLogs.Size = New System.Drawing.Size(310, 575)
+        Me.pnlLogs.TabIndex = 3
+        '
+        'lstLogs
+        '
+        Me.lstLogs.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lstLogs.BackColor = System.Drawing.Color.FromArgb(CType(CType(10, Byte), Integer), CType(CType(15, Byte), Integer), CType(CType(20, Byte), Integer))
+        Me.lstLogs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lstLogs.Font = New System.Drawing.Font("Consolas", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstLogs.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(230, Byte), Integer), CType(CType(200, Byte), Integer))
+        Me.lstLogs.Location = New System.Drawing.Point(15, 75)
+        Me.lstLogs.Multiline = True
+        Me.lstLogs.Name = "lstLogs"
+        Me.lstLogs.ReadOnly = True
+        Me.lstLogs.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.lstLogs.Size = New System.Drawing.Size(283, 480)
+        Me.lstLogs.TabIndex = 6
+        '
+        'lblLogsTitle
+        '
+        Me.lblLogsTitle.AutoSize = True
+        Me.lblLogsTitle.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLogsTitle.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(215, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblLogsTitle.Location = New System.Drawing.Point(88, 37)
+        Me.lblLogsTitle.Name = "lblLogsTitle"
+        Me.lblLogsTitle.Size = New System.Drawing.Size(152, 21)
+        Me.lblLogsTitle.TabIndex = 5
+        Me.lblLogsTitle.Text = "(CONSOLA VISUAL)"
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(663, 504)
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1288, 670)
+        Me.Controls.Add(Me.pnlMain)
+        Me.Controls.Add(Me.pnlLogs)
+        Me.Controls.Add(Me.pnlSidebar)
+        Me.Controls.Add(Me.pnlHeader)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MinimumSize = New System.Drawing.Size(1050, 620)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "🏆🏆🏆 TOURNAMENT SORT - UPRIT 🏆🏆🏆"
+        Me.pnlHeader.ResumeLayout(False)
+        Me.pnlHeader.PerformLayout()
+        Me.pnlSidebar.ResumeLayout(False)
+        Me.pnlSidebar.PerformLayout()
+        Me.pnlMain.ResumeLayout(False)
+        Me.pnlMain.PerformLayout()
+        Me.pnlLogs.ResumeLayout(False)
+        Me.pnlLogs.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+
 
 End Class
--- TournamentSort/Form1.vb
+++ TournamentSort/Form1.vb
@@ -1,3 +1,175 @@
 ﻿Public Class Form1
 
+    ' Estructura para representar a cada país competidor
+    Public Structure Pais
+        Public Nombre As String
+        Public Poblacion As Integer
+
+        Public Sub New(nombre As String, poblacion As Integer)
+            Me.Nombre = nombre
+            Me.Poblacion = poblacion
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"{Nombre} ({Poblacion.ToString("N0")} hab.)"
+        End Function
+    End Structure
+
+    Private participantes() As Pais
+
+    ' Método para cargar la lista inicial de países participantes
+    Private Sub btnGenerateData_Click(sender As Object, e As EventArgs) Handles btnGenerateData.Click
+        lstCountries.Items.Clear()
+        tvTournament.Nodes.Clear()
+        lstLogs.Clear()
+
+        lstLogs.AppendText("[SISTEMA] Cargando base de datos de países..." & vbCrLf)
+
+        participantes = New Pais() {
+            New Pais("Perú", 34000000),
+            New Pais("Japón", 125000000),
+            New Pais("Canadá", 38000000),
+            New Pais("Egipto", 109000000),
+            New Pais("Brasil", 214000000),
+            New Pais("Italia", 59000000),
+            New Pais("Australia", 25000000),
+            New Pais("India", 1400000000)
+        }
+
+        For Each p As Pais In participantes
+            lstCountries.Items.Add(p)
+        Next
+
+        lstLogs.AppendText("[SISTEMA] ¡8 países registrados y listos para competir!" & vbCrLf)
+    End Sub
+
+    ' Valida e inicia la ejecución del torneo
+    Private Sub btnStartTournament_Click(sender As Object, e As EventArgs) Handles btnStartTournament.Click
+        If participantes Is Nothing OrElse participantes.Length = 0 Then
+            MessageBox.Show("¡Primero debes cargar los países!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        lstLogs.Clear()
+        tvTournament.Nodes.Clear()
+        lstLogs.AppendText("====================================" & vbCrLf)
+        lstLogs.AppendText("🏆 INICIA EL TOURNAMENT SORT 🏆" & vbCrLf)
+        lstLogs.AppendText("====================================" & vbCrLf)
+
+        EjecutarTournamentSort(participantes)
+    End Sub
+
+    ' Aquí se maneja la lógica de recorrido para el ranking de países
+    Private Sub EjecutarTournamentSort(arregloOriginal() As Pais)
+        Dim competidores As New List(Of Pais)(arregloOriginal)
+        Dim listaOrdenada As New List(Of Pais)
+        Dim nodoRaiz As New TreeNode("🏆 TORNEO MUNDIAL 🏆")
+        tvTournament.Nodes.Add(nodoRaiz)
+
+        Dim ronda As Integer = 1
+
+        While competidores.Count > 0
+            Dim nodoRonda As TreeNode = Nothing
+
+            If ronda = 1 Then
+                lstLogs.AppendText(vbCrLf & "--- 🎬 GRAN TORNEO: BUSCANDO AL CAMPEÓN ---" & vbCrLf)
+                nodoRonda = New TreeNode("Llaves para el 1er Puesto")
+                nodoRaiz.Nodes.Add(nodoRonda)
+            End If
+
+            Dim narrarPartidos As Boolean = (ronda = 1)
+            Dim ganadorActual As Pais = TorneoRecursivo(competidores, nodoRonda, narrarPartidos)
+
+            ' Gestión visual del podio y ranking final
+            If ronda = 1 Then
+                lstLogs.AppendText(vbCrLf & $"🥇 CAMPEÓN (1er Puesto): {ganadorActual.Nombre}!" & vbCrLf)
+                lstLogs.AppendText("--- CALCULANDO RESTO DEL RANKING... ---" & vbCrLf)
+            ElseIf ronda = 2 Then
+                lstLogs.AppendText($"🥈 2do Puesto: {ganadorActual.Nombre}" & vbCrLf)
+            ElseIf ronda = 3 Then
+                lstLogs.AppendText($"🥉 3er Puesto: {ganadorActual.Nombre}" & vbCrLf)
+            Else
+                lstLogs.AppendText($"🏅 {ronda}to Puesto: {ganadorActual.Nombre}" & vbCrLf)
+            End If
+
+            listaOrdenada.Add(ganadorActual)
+            competidores.Remove(ganadorActual)
+            ronda += 1
+        End While
+
+        lstLogs.AppendText("====================================" & vbCrLf)
+        lstLogs.AppendText("✅ ORDENAMIENTO COMPLETADO ✅" & vbCrLf)
+
+        tvTournament.ExpandAll()
+        lstCountries.Items.Clear()
+        lstCountries.Items.Add("--- RANKING FINAL ---")
+        For Each p As Pais In listaOrdenada
+            lstCountries.Items.Add(p)
+        Next
+    End Sub
+
+    ' Lógica recursiva para los enfrentamientos por llaves
+    Private Function TorneoRecursivo(listaActual As List(Of Pais), nodoPadre As TreeNode, narrar As Boolean) As Pais
+        If listaActual.Count = 1 Then
+            Return listaActual(0)
+        End If
+
+        Dim ganadoresSiguienteRonda As New List(Of Pais)
+
+        For i As Integer = 0 To listaActual.Count - 1 Step 2
+            If i + 1 >= listaActual.Count Then
+                Dim avanzaSolo As Pais = listaActual(i)
+                If narrar Then lstLogs.AppendText($"  [Pase Directo] {avanzaSolo.Nombre} avanza sin rival." & vbCrLf)
+                If nodoPadre IsNot Nothing Then nodoPadre.Nodes.Add($"Pase Directo: {avanzaSolo.Nombre}")
+                ganadoresSiguienteRonda.Add(avanzaSolo)
+            Else
+                Dim pais1 As Pais = listaActual(i)
+                Dim pais2 As Pais = listaActual(i + 1)
+                Dim ganadorPartido As Pais
+
+                ' Comparación por población para determinar el ganador
+                If pais1.Poblacion >= pais2.Poblacion Then
+                    ganadorPartido = pais1
+                Else
+                    ganadorPartido = pais2
+                End If
+
+                If narrar Then
+                    lstLogs.AppendText($"  ⚔️ {pais1.Nombre} vs {pais2.Nombre} --> Gana: {ganadorPartido.Nombre}" & vbCrLf)
+                End If
+
+                If nodoPadre IsNot Nothing Then
+                    Dim nodoPartido As New TreeNode($"{pais1.Nombre} vs {pais2.Nombre} => Avanza {ganadorPartido.Nombre}")
+                    nodoPadre.Nodes.Add(nodoPartido)
+                End If
+
+                ganadoresSiguienteRonda.Add(ganadorPartido)
+            End If
+        Next
+
+        Return TorneoRecursivo(ganadoresSiguienteRonda, nodoPadre, narrar)
+    End Function
+
+    ' Configuración de la interfaz y elementos institucionales
+    Protected Overrides Sub OnLoad(ByVal e As EventArgs)
+        MyBase.OnLoad(e)
+        Try
+            lstLogs.Multiline = True
+            lstLogs.WordWrap = True
+            lstLogs.ScrollBars = ScrollBars.Vertical
+
+            Dim webClient As New System.Net.WebClient()
+            Dim iconData As Byte() = webClient.DownloadData("https://uprit.edu.pe/favicon/apple-touch-icon.png")
+            Using ms As New System.IO.MemoryStream(iconData)
+                Me.Icon = New System.Drawing.Icon(ms)
+            End Using
+        Catch ex As Exception
+            Me.Text = "🏆 TOURNAMENT SORT - UPRIT 🏆"
+        End Try
+    End Sub
+
+    Private Sub lblGroup_Click(sender As Object, e As EventArgs) Handles lblGroup.Click
+        ' Evento de etiqueta de grupo
+    End Sub
+
 End Class
